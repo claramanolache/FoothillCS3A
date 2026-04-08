2255158483f3b735954df6ffdb4458cf90e8c437
Clara Manolache <clara.manolache@gmail.com>
2026-04-08 16:39:13 -0700

    start of proj 3 + renamed previous proj

---
+++ week3 (menu)/lab_assignment 3.py
@@ -0,0 +1,10 @@
+"""
+Prints the menu
+"""
+def print_menu():
+    print("Main Menu")
+    print("---------")
+    print("1 - Process a new data file\n2 - Choose units\n3 - Edit room filter")
+    print("4 - Show summary statistics\n5 - Show temperature by date and time")
+    print("6 - Show histogram of temperatures\n7 - Quit")
+
