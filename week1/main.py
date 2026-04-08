7ae75b411fedce44342e6a11454bbab9d84ed1f5
Clara Manolache <clara.manolache@gmail.com>
2026-04-08 00:25:01 -0700

    first project done + submited

---
+++ week1/main.py
@@ -0,0 +1,27 @@
+"""
+Assignment 1: Opening Lines
+Submitted by Mike Murphy
+Submitted:  January 11, 2026
+
+Assignment 1: This program demonstrates printing lines of text to the screen
+"""
+import sys
+
+
+def print_header():
+    print("STEM Center Temperature Project")
+    print("Clara Manolache")
+
+def main():
+    print_header()
+
+if __name__ == "__main__":
+    sys.exit(main())
+
+"""
+/usr/bin/python3 /Users/claramanolache/FoothillCS3A/week1/main.py 
+STEM Center Temperature Project
+Clara Manolache
+
+Process finished with exit code 0
+"""
