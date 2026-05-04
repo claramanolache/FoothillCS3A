dfc907548125c7c2ff210dea4ed1aff7f5cf0767
Clara Manolache <clara.manolache@gmail.com>
2026-05-03 22:42:22 -0700

    1st way of making senor list

--- week 4/lab_assigment#4.py
+++ week 4/lab_assigment#4.py
@@ -2,7 +2,22 @@ import sys
 
 
 def main():
-    print("working")
+    sensors = {
+        "4213": ("STEM Center", 0),
+        "4201": ("Foundations Lab", 1),
+        "4204": ("CS Lab", 2),
+        "4218": ("Workshop Room", 3),
+        "4205": ("Tiled Room", 4),
+        "Out": ("Outside", 5)
+    }
+    sensors_list = []
+    # loading sensor list
+    keys = list(sensors.keys())
+    for i in range (6):
+        room_num = keys[i]
+        desc = sensors.get(room_num)[0]
+        sens_num = sensors.get(room_num)[1]
+        sensors_list.append((room_num, desc, sens_num))
 
 if __name__ == "__main__":
     sys.exit(main())
